36981e09fa8d1fac32df94d49f9cb8bd9c721705
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-24 00:41:54 +0500

    create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
-
+import Contact from './pages/Contact'
+import Doctor from './pages/Doctors'
+import MyProfile from './pages/MyProfile'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
      <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:specialty' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/myprofile' element={<MyProfile />} />
+ <Route path='/myappointments' element={<MyAppointments />} />
+      <Route path='/appointments/:docId' element={<Appointment/>} />
       </Routes> 
     </div>
   )
