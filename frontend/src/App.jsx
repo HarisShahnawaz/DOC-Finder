c415a1cdd50e4ab06c13316e29bfd27bf9a705bb
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-24 00:25:55 +0500

    new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Routes } from 'react-router-dom'
 const App = () => {
-
+import Home from './pages/Home'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Contact from './pages/Contact'
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/my-appointments' element={<MyAppointments />} />
+      <Route path='/my-profile' element={<MyProfile />} />
+      <Route path='/contact' element={<Contact />} />
+      </Routes> 
     </div>
   
   )
