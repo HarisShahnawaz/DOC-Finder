ccf499ed2576a5fd1048b92fea843abd8069a45b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-26 18:12:04 +0500

    edit navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import {assets} from '../assets/assets_frontend/assets'
 import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const[showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-4 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
@@ -24,8 +31,21 @@ const Navbar = () => {
          </NavLink>
          
         </ul>
-        <div>
-          <button>Create Account</button>
+        <div className='flex items-center gap-4'>
+          {
+            token ? <div className='flex items-center gap-2 cursor-pointer group relative' >
+              <img className='w-8  rounded-full' src={assets.profile_pic} alt="" />
+              <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={() => navigate('/myprofile')} className='cursor-pointer hover:text-black'>My Profile</p>
+                  <p onClick={() => navigate('/myappointments')} className='cursor-pointer hover:text-black'>My Appointments</p>
+                  <p onClick={() => navigate('/login')} className='cursor-pointer hover:text-black'>Logout</p>
+                </div>
+              </div>
+            </div> :  <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Account</button>
+          }
+         
         </div>
     </div>
     
