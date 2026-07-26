68b789961a7978ab897bdc11501c2d6a6fb37236
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-26 20:36:54 +0500

    NEW FILES ADDED

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,22 +5,22 @@ const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap  bg-primary px-6 md:px-10 lg:px-20 rounded-lg'>
         {/*--------left side --------- */}
-       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
-        <p>
+       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-7.5'>
+        <p className='text-3xl md:text-4xl font-semibold text-white leading-tight md:leading-tight lg:leading-tight'>
             Book Appointments <br /> With Trusted Doctors.
         </p>
-        <div>
-            <img src={assets.group_profiles} alt="" />
-            <p>Simply browse through our extensive list of trusted doctors, <br />  schedule your appointments with ease.</p>
+        <div className='flex flex-col md:flex-row items-start md:items-center gap-3 text-white text-sm font-light'>
+            <img className='w-28' src={assets.group_profiles} alt="" />
+            <p>Simply browse through our extensive list of  doctors, <br className='hidden sm:block' />  schedule your appointments with ease.</p>
         </div>
-        <a href="">
-            Book Appointment <img src={assets.arrow_icon} alt="" />
+        <a className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href="#speciality">
+            Book Appointment <img className='w-3' src={assets.arrow_icon} alt="" />
         </a>
        </div>
 
          {/*--------right side --------- */}
-         <div>
-          <img src={assets.header_img} alt="" />
+         <div className='md:w-1/2 relative'>
+          <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
          </div>
 
     </div>
