d1369b2098c7216221ac35cba7281e3fd1061f78
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-27 22:22:25 +0500

    create appcontext

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { doctors } from '../assets/assets_frontend/assets'
-
+import { useNavigate } from 'react-router-dom'
 const TopDoctors = () => {
+  const navigate = useNavigate()
+
   return (
     <div className='flex flex-col gap-4 items-center my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors To Book</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0, 10).map((item, index) => (
-                <div className='border border-blue-200 overflow-hidden rounded-xl cursor-pointer hover:translate-y-2.5 transition-all duration-500' key={index}>
+                <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 overflow-hidden rounded-xl cursor-pointer hover:translate-y-2.5 transition-all duration-500' key={index}>
                     <img className='bg-blue-50 ' src={item.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center text-sm gap-2 text-center text-green-500'>
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { doctors } from '../assets/assets_frontend/assets'
+import React from "react";
+
+export const AppContext = createContext()
+
+export const AppContextProvider = (props) => {
+
+    const value = {
+        doctors
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
