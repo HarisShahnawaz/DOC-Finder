7163c77e6f8058f826e335c6a07e3478ad276a2d
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-27 22:02:46 +0500

    edit top doctors

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -5,24 +5,25 @@ const TopDoctors = () => {
   return (
     <div className='flex flex-col gap-4 items-center my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors To Book</h1>
-        <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of  doctors.</p>
-         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-            {doctors.slice(0,10).map((item,index)=>(
-                <div className='border border-blue-200 overflow-hidden rounded-xl cursor-pointer' key={index}>
-                    <img className='bg-blue-50' src={item.image} alt="" />
+        <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of doctors.</p>
+        <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {doctors.slice(0, 10).map((item, index) => (
+                <div className='border border-blue-200 overflow-hidden rounded-xl cursor-pointer hover:translate-y-2.5 transition-all duration-500' key={index}>
+                    <img className='bg-blue-50 ' src={item.image} alt="" />
                     <div className='p-4'>
-                        <div className='flex items-center text-sm gap-2 text-center text-green-500 '>
-                        <p className='w-2 h-2 bg-green-500 rounded-full'></p> <p>Available</p>
+                        <div className='flex items-center text-sm gap-2 text-center text-green-500'>
+                            <p className='w-2 h-2 bg-green-500 rounded-full'></p>
+                            <p>Available</p>
                         </div>
+                        <p className='text-gray-900 text-base font-medium'>{item.name}</p>
+                        <p className='text-gray-600 text-sm'>{item.speciality}</p>
                     </div>
-                   <p>{item.name}</p>
-                   <p>{item.speciality}</p>
                 </div>
             ))}
-         </div>
-         <button>
-            more
-         </button>
+        </div>
+        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>
+            More
+        </button>
     </div>
   )
 }
