587c596e10c62b82540e0566f66b89ce24bbd247
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-07-29 22:16:28 +0500

    doctors page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -36,7 +36,7 @@ const Doctors = () => {
           <p>Neurologist</p>
           <p>Gastroentrologist</p>
         </div>
-        <div className='w-full grid grid-cols-auto gap-3 gap-y-10'>
+        <div className='w-full grid grid-cols-auto gap-7.5 gap-y-6 '>
             {
               filterDoc.map((item, index) => (
                 <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 overflow-hidden rounded-xl cursor-pointer hover:translate-y-2.5 transition-all duration-500' key={index}>
