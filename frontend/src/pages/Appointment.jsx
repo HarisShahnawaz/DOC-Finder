976a144ed45fb6b7b3b9f0c544b4764db9e7115f
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-08-02 22:12:24 +0500

    set date and time in appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -10,15 +10,37 @@ const Appointment = () => {
   const { doctors , currencySymbol } = useContext(AppContext)
 
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
-    setDocInfo(docInfo)
+    
+  }
+
+  const getAvailableSlots = async () => {
+     setDocSlots( [])
+
+     //getting current date
+     let today = new Date()
+
+     for (let i = 0; i < 7; i++) {
+      //getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate() + i)
+
+      //setting endtime of the date with index
+     }
   }
 
   useEffect(() => {
     fetchDocInfo()
   }, [doctors, docId])
+
+  useEffect(() => {
+    getAvailableSlots()
+  },[docInfo])
 
   return docInfo ? (
     <div>
