33598a7fc9c6297bc54b15972b012ec1af366a29
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-08-02 22:36:44 +0500

    set date and time in appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -16,11 +16,11 @@ const Appointment = () => {
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
-    
+    setDocInfo(docInfo)
   }
 
   const getAvailableSlots = async () => {
-     setDocSlots( [])
+     setDocSlots([])
 
      //getting current date
      let today = new Date()
@@ -31,7 +31,34 @@ const Appointment = () => {
       currentDate.setDate(today.getDate() + i)
 
       //setting endtime of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate() + i)
+      endTime.setHours(21, 0, 0, 0)
+      //setting hours
+      if(today.getDate() === currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      }
+      else
+      {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+
+      }
+      let timeSlots = []
+      while(currentDate < endTime){
+        let  formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+        //incrementing time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+      setDocSlots(prev => [...prev, timeSlots])
      }
+
   }
 
   useEffect(() => {
@@ -41,6 +68,11 @@ const Appointment = () => {
   useEffect(() => {
     getAvailableSlots()
   },[docInfo])
+
+  useEffect(() => {
+    console.log(docSlots);
+    
+  },[docSlots])
 
   return docInfo ? (
     <div>
@@ -53,16 +85,16 @@ const Appointment = () => {
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-20 sm:mt-0'>
            {/*-------- Doctor Information --------- */}
            <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /></p>
-           < div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+           <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
             <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience} </button>
            </div>
- {/*-------- Doctor About --------- */}
+           {/*-------- Doctor About --------- */}
            <div>
-            < p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
             <p className='text-sm text-gray-600 max-w-175 mt-1 '>{docInfo.about}</p>
            </div>
-           <p className='text-gray-500 font-medium mt-4'>Appointment Fee: <span className='text-gray-600'>{ currencySymbol}{docInfo.fees}</span></p>
+           <p className='text-gray-500 font-medium mt-4'>Appointment Fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span></p>
         </div>
       </div>  
     </div>
