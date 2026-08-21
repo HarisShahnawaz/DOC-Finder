25aa595c5c3d25a9c5c335d0275b330cbb0a113a
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-08-21 20:26:08 +0500

    start woeking on myprofile page

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -40,7 +40,7 @@ const MyProfile = () => {
               ? <input type='text' value={userData.phone} onChange={e => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
               : <p>{userData.phone}</p>
           }
-          <p>addressss</p>
+          <p>addressssss</p>
 
 
         </div>
