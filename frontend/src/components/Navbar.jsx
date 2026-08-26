718ca3e5057c39205baef0479add90d0947b763f
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-08-26 21:17:17 +0500

    fix navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets} from '../assets/assets_frontend/assets'
+import { assets } from '../assets/assets_frontend/assets'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
@@ -7,48 +7,62 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
-  const[showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const [token, setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-4 border-b border-b-gray-400'>
-        <img onClick={()=>{navigate('/')}} className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
-        <ul className='hidden md:flex items-start gap-5 font-medium'>
-         <NavLink to='/'>
+      <img onClick={() => { navigate('/') }} className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink to='/'>
           <li className='py-1'>Home</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-         </NavLink>
-          <NavLink to='/doctors'>
+        </NavLink>
+        <NavLink to='/doctors'>
           <li className='py-1'>All Doctors</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-         </NavLink>
-          <NavLink to='/about'>
+        </NavLink>
+        <NavLink to='/about'>
           <li className='py-1'>About</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-         </NavLink>
-          <NavLink to='/contact'>
+        </NavLink>
+        <NavLink to='/contact'>
           <li className='py-1'>Contact</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-         </NavLink>
-         
-        </ul>
-        <div className='flex items-center gap-4'>
-          {
-            token ? <div className='flex items-center gap-2 cursor-pointer group relative' >
-              <img className='w-8  rounded-full' src={assets.profile_pic} alt="" />
-              <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                  <p onClick={() => navigate('/my-profile')} className='cursor-pointer hover:text-black'>My Profile</p>
-                  <p onClick={() => navigate('/my-appointments')} className='cursor-pointer hover:text-black'>My Appointments</p>
-                  <p onClick={()=> setToken(false)}  className='cursor-pointer hover:text-black'>Logout</p>
-                </div>
+        </NavLink>
+
+      </ul>
+      <div className='flex items-center gap-4'>
+        {
+          token ? <div className='flex items-center gap-2 cursor-pointer group relative' >
+            <img className='w-8  rounded-full' src={assets.profile_pic} alt="" />
+            <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <p onClick={() => navigate('/my-profile')} className='cursor-pointer hover:text-black'>My Profile</p>
+                <p onClick={() => navigate('/my-appointments')} className='cursor-pointer hover:text-black'>My Appointments</p>
+                <p onClick={() => setToken(false)} className='cursor-pointer hover:text-black'>Logout</p>
               </div>
-            </div> :  <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Account</button>
-          }
-         
+            </div>
+          </div> : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Account</button>
+        }
+        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+        {/*Mobile Menu */}
+        <div>
+          <div>
+            <img src={assets.logo} alt="" />
+            <img onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul>
+            <NavLink>Home</NavLink>
+            <NavLink>All Doctors</NavLink>
+            <NavLink>Contact</NavLink>
+            <NavLink>About</NavLink>
+          </ul>
         </div>
+      </div>
+
     </div>
-    
+
   )
 }
 
