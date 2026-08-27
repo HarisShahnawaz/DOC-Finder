59f0bc8bc9d29e470427233c94233e1e52437c97
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-08-27 20:27:10 +0500

    frontend completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
 const Footer = () => {
-  return (
-    <div className='md:mx-10'>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-               {/*--------left side --------- */}
+    return (
+        <div className='md:mx-10'>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                {/*--------left side --------- */}
                 <div>
-                  <img className='mb-5 w-40' src={assets.logo} alt="" />
-                  <p className='w-full md:w-1/2 text-gray-600 leading-6'>Find and hire trusted, verified doctors near you — fast, simple, and hassle-free. Compare profiles, check credentials, and book with confidence. Your health, matched with the right care.</p>
+                    <img className='mb-5 w-40' src={assets.logo} alt="" />
+                    <p className='w-full md:w-1/2 text-gray-600 leading-6'>Find and hire trusted, verified doctors near you — fast, simple, and hassle-free. Compare profiles, check credentials, and book with clear confidence. Your health, matched with the right care.</p>
                 </div>
                 {/*--------middle side --------- */}
                 <div>
@@ -27,14 +27,14 @@ const Footer = () => {
                         <li>harisshahnawaz97@gmail.com</li>
                     </ul>
                 </div>
+            </div>
+            {/*--------last section --------- */}
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2026@ Doc-Finder - All Right Reserved.</p>
+            </div>
         </div>
-         {/*--------last section --------- */}
-        <div>
-           <hr />
-           <p className='py-5 text-sm text-center'>Copyright 2026@ Doc-Finder - All Right Reserved.</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
