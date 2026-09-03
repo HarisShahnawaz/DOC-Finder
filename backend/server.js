0bf015503fbd452fbd5c222b652d812a13aaa5b9
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-03 17:49:15 +0500

    create backend files

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-const a = require('express')();
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config.js';
+
+
+// app config 
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
+
+app.get('/', (req, res) => {
+  res.send('Api Working');
+});
