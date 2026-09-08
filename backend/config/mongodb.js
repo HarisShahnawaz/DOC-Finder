242259b92dcab8501ae81c91fb5345481e3feb89
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-08 21:03:24 +0500

    set mongodb

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,4 +1,15 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
