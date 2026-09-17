efd54a62eacad53246af23faa3ee14515aa5e098
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-17 20:45:56 +0500

    fix mongodb issue

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Force Node to use Google DNS instead of the router's DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', ()=> console.log('MongoDB connected'));
-      await mongoose.connect(`${process.env.MONGODB_URI}/docfinder`) 
+    mongoose.connection.on('connected', () => console.log('MongoDB connected'));
+    await mongoose.connect(`${process.env.MONGODB_URI}/docfinder`);
 }
 
 export default connectDB;
