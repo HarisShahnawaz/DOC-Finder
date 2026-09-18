8a8b59b31c4519c21f150d8f13763c1ecd84cc87
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-18 21:33:47 +0500

    add admin routes

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,8 @@
 import validator from "validator"
 import bcrypt from  "bcrypt"
 import {v2 as cloudinary} from "cloudinary" 
+import doctorModel from "../models/doctorModel.js"
+import jwt from "jsonwebtoken"
 //Api for adding doctor
 
 const addDoctor = async (req, res) => {
@@ -52,14 +54,40 @@ const addDoctor = async (req, res) => {
         date: Date.now(),
 
     }
+    const newDoctor = new doctorModel(doctorData); 
+    await newDoctor.save();
+    res.json({success:true , message: "Doctor added successfully" });
     }
 
    
 
     catch(error){
+        console.log(error);
+        res.json({success:false , message:error.message || "Internal Server Error" });
         
     }
+    
 }
 
+  // api for admin login
 
-export { addDoctor };
+  const loginAdmin = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+           
+            const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.json({success:true ,token });
+        }
+        else{
+            res.json({success:false , message: "Invalid credentials" });
+        }
+    }
+    catch(error){
+         console.log(error);
+        res.json({success:false , message:error.message || "Internal Server Error" });
+    }
+  }
+
+
+export { addDoctor, loginAdmin };
