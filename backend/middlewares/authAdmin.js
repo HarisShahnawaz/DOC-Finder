28de3700dbb39a6e309a3a65c84817a26964ac29
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-19 21:00:23 +0500

    update

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -9,10 +9,11 @@ const authAdmin = (req, res, next) => {
         }
 
         const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
-       if(token_decode){
-        req.admin = token_decode;
-        next();
+       if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+        return res.json({success:false , message: "Not Authorized login again" });
+        
        }
+       next();
     }
     catch(error){
        console.log(error);
