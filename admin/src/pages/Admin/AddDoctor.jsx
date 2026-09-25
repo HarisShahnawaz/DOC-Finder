17ddeefed08dab30d30b208b5b96f3787998b98a
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-25 20:38:03 +0500

    add doctot page

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -31,11 +31,71 @@ const AddDoctor = () => {
           </div>
 
           <div>
-
+            <p>Doctor Password</p>
+            <input type='password' placeholder='Password' required />
           </div>
 
 
+          <div>
+            <p>Experience</p>
+            <select name='' id=''>
+              <option value="1 year"> 1 year </option>
+              <option value="2 year"> 2 year </option>
+              <option value="3 year"> 3 year </option>
+              <option value="4 year"> 4 year </option>
+              <option value="5 year"> 5 year </option>
+              <option value="6 year"> 6 year </option>
+              <option value="7 year"> 7 year </option>
+              <option value="8 year"> 8 year </option>
+              <option value="9 year"> 9 year </option>
+              <option value="10 year"> 10 year </option>
+
+
+            </select>
+          </div>
+
+          <div>
+            <p>Fees</p>
+            <input type='number' placeholder='fees' required />
+          </div>
+
         </div>
+
+        <div>
+          <div>
+            <div>
+              <p>Speciality</p>
+              <select name="" id="">
+                <option value="General Physician">General Physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroentrologist">Gastroentrologist</option>
+              </select>
+            </div>
+
+            <div>
+              <p>Education</p>
+              <input type='text' placeholder='Education' required />
+            </div>
+
+            <div>
+              <p>Address</p>
+              <input type='text' placeholder='address 1' required />
+              <input type='text' placeholder='address 2' required />
+
+            </div>
+
+
+          </div>
+        </div>
+
+        <div>
+          <p>About me</p>
+          <textarea type="text" placeholder='write about doctor' />
+        </div>
+
 
       </div>
 
