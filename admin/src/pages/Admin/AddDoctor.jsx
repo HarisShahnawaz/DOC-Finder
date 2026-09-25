10875feeca17bdc4254cf7a93226301740cce281
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-09-25 21:43:55 +0500

    add css styles

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -3,7 +3,7 @@ import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
 
-  const [docImg, setDocImg] = useState('false')
+  const [docImg, setDocImg] = useState(false)
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
